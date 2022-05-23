737c05520427ee4128c69b45dd002e7168e17528
MateCon <mateoziffer@gmail.com>
2022-05-23 10:31:20 -0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,65 @@
+let todos = [];
+let index = todos.length; // para tener un id autoincremental
+let shortestTodo = null;
+
+const createTodoElement = (todo, todos, container) => {
+    const element = document.createElement("div");
+    const text = document.createElement("p");
+    text.innerHTML = todo.value;
+    element.appendChild(text);
+    element.classList.add("todo");
+    if (todo.isCompleted) element.classList.add("completed");
+    element.addEventListener("click", () => {
+        todo.isCompleted = !todo.isCompleted;
+        if (todo.isCompleted) {
+            console.log(new Date() - todo.creationDate);
+            console.log();
+            if (!shortestTodo || new Date() - todo.creationDate < shortestTodo.completionDate - shortestTodo.creationDate)
+                shortestTodo = todo;
+            todo.completionDate = new Date();
+        }
+        element.classList.toggle("completed");
+        console.log(shortestTodo);
+    });
+    const cross = document.createElement("span");
+    cross.innerHTML = "X";
+    cross.addEventListener("click", () => {
+        for (let i = 0; i < todos.length; i++) {
+            if (todos[i].id === todo.id) {
+                todos.splice(i, 1)
+                loadTodos(todos, container);
+            }
+        }
+    });
+    element.appendChild(cross);
+    return element;
+}
+
+const loadTodos = (todos, container) => {
+    container.innerHTML = null;
+    const elements = todos.map(todo => createTodoElement(todo, todos, container));
+    elements.forEach(element => container.appendChild(element));
+    // scroll to bottom
+    const dummy = document.createElement("div");
+    container.appendChild(dummy);
+    dummy.scrollIntoView({ behavior: "smooth" });
+}
+
+document.getElementById("todo-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!e.target[0].value) return;
+
+    todos.push({
+        value: e.target[0].value,
+        isCompleted: false,
+        creationDate: new Date(),
+        completionDate: null,
+        id: index
+    });
+    index++;
+    e.target[0].value = "";
+
+    const container = document.getElementById("todo-container");
+    if (!container) return;
+    loadTodos(todos, container);
+});
